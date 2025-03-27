26d02d474e3e697bec7da43f40e5d988779b9b00
rupali-98 <84447894+rupali-98@users.noreply.github.com>
2025-03-27 12:05:23 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ import cv2
 import numpy as np
 from utils import noise_reduction,line_detection,morph_operation_to_get_line,refine_boundary
 ### Getting Boundary ##
-def boundary(room_img,saved_file):
+def boundary(room_img,area_min,saved_file):
     room_img = cv2.imread(room_img)
     noise_reduced_img = noise_reduction(room_img)
     line_Detection = line_detection(noise_reduced_img)
     morphed_img = morph_operation_to_get_line(line_Detection)
-    refined_boundary = refine_boundary(morphed_img)
+    refined_boundary = refine_boundary(morphed_img,area_min)### area_min: adjuested according to different area of contour you want to include
     cv2.imwrite(saved_file,refined_boundary)
     print("Saved file")
     return refined_boundary
 if __name__ == "__main__":
     room1_path = 'room1.pgm'  # Replace with your image path
-    boundary(room1_path,"Room1_wall.png")
+    boundary(room1_path,10000,"Room1_wall.png")### area_min: adjuested according to different area of contour you want to include
     room2_path = 'room2.pgm'  # Replace with your image path
-    boundary(room2_path,"Room2_wall.png")
+    boundary(room2_path,10000,"Room2_wall.png")
     room3_path = 'room3.pgm'  # Replace with your image path
-    boundary(room3_path,"Room3_wall.png")
+    boundary(room3_path,10000,"Room3_wall.png")
--- utils.py
+++ utils.py
@@ -4,23 +4,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+
 def noise_reduction(img):
-    blurred = cv2.medianBlur(img, 3) ## Removal of Salt and pepper noise
-    _, binary = cv2.threshold(blurred, 127, 255, cv2.THRESH_BINARY)  # Create binary image.
+    blurred = cv2.medianBlur(img, 3)  ## Removal of Salt and pepper noise
+    # _, binary = cv2.threshold(blurred, 127, 255, cv2.THRESH_BINARY)  # Create binary image.
     # kernel = np.ones((2, 2), np.uint8)  # Define the structuring element
     # closed = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel, iterations=2) # Apply Closing
     kernel = np.ones((2, 2), np.uint8)
     eroded = cv2.morphologyEx(blurred, cv2.MORPH_ERODE, kernel, iterations=2)  # Apply erosion
     return eroded
 
+
 def line_detection(img):
-    high_thresh, thresh_im = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    lowThresh = 0.5*high_thresh
+    high_thresh, thresh_im = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 0, 255,
+                                           cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    lowThresh = 0.5 * high_thresh
     ## 1. Canny Edge Detector
     edges = cv2.Canny(img, lowThresh, high_thresh)
     kernel = np.ones((2, 2), np.uint8)
     ## 2. Dilating the edges
-    morph = cv2.morphologyEx(edges, cv2.MORPH_DILATE, kernel) ## Dilate the edges
+    morph = cv2.morphologyEx(edges, cv2.MORPH_DILATE, kernel)  ## Dilate the edges
     # 3. Hough Transform with Adjusted Parameters
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     lines = cv2.HoughLinesP(morph, 1, np.pi / 180, 30, minLineLength=20, maxLineGap=15)  # Adjusted parameters
@@ -41,12 +44,15 @@ def line_detection(img):
             cv2.line(floor_plan, (x1, y1), (x2, y2), 255, 2)
     return floor_plan
 
+
 def morph_operation_to_get_line(img):
     # Morphological Operations
     kernel = np.ones((3, 3), np.uint8)
     morph = cv2.morphologyEx(img, cv2.MORPH_DILATE, kernel)
-    morph2 = cv2.morphologyEx(morph, cv2.MORPH_OPEN, kernel,iterations=2)
-    return morph2
+    # morph2 = cv2.morphologyEx(morph, cv2.MORPH_OPEN, kernel,iterations=2)
+
+    return morph
+
 
 # def contouring(combined_lines,morph):
 #     # gray_morph = cv2.cvtColor(morph, cv2.COLOR_BGR2GRAY)
@@ -95,12 +101,13 @@ def morph_operation_to_get_line(img):
 #           cv2.line(morph, cl[0][0], cl[0][1], (255, 255, 255), thickness=10)
 #     return morph
 
-def refine_boundary(img):
+def refine_boundary(img, area_min):
+    ###area_min: Minimum area for contour detection##
     contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     refined_img = np.zeros_like(img)
     for contour in contours:
         # Check if the contour is closed (area > 0)
-        if cv2.contourArea(contour) > 1000:
+        if cv2.contourArea(contour) > area_min:
             # Approximate the contour with a polygon
             epsilon = 0.0005 * cv2.arcLength(contour, True)  # Adjust epsilon for accuracy
             approx = cv2.approxPolyDP(contour, epsilon, True)
